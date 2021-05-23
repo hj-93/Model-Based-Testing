7694d2e13fe340a6e8b3d87700c8e97211ad4470
Hui Jing <h.jingict@gmail.com>
2021-05-23 20:17:06 +0200

    add properties

--- src/test/java/com/luugiathuy/games/reversi/PropertyTest.java
+++ src/test/java/com/luugiathuy/games/reversi/PropertyTest.java
@@ -11,8 +11,22 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
+
 @RunWith(JUnitQuickcheck.class)
 public class PropertyTest {
+    private int getTotalPieceCount(Reversi r) {
+        int totalPieces = 0;
+        char[][] board = r.getBoard();
+        for (int i = 0; i < Reversi.sBOARD_SIZE; ++i) {
+            for (int j = 0; j < Reversi.sBOARD_SIZE; ++j) {
+                if(board[i][j] == Reversi.sBLACK_PIECE || board[i][j] == Reversi.sWHITE_PIECE) {
+                    ++totalPieces;
+                }
+            }
+        }
+        return totalPieces;
+    }
+
     @Property public void PlayerMoveAvailableGameNotEnded(@From(ReversiGenerator.class) Reversi r) {
         char piece = (r.mIsBlackTurn) ? r.sBLACK_PIECE : r.sWHITE_PIECE;
         boolean playerMoveNotAvailable = (r.findValidMove(r.mBoard, piece, true)).isEmpty();
@@ -20,9 +34,27 @@ public class PropertyTest {
             assertEquals(r.mState, r.PLAYING);
         }
     }
-//Hui
+
     //OneMovePlayedOnePieceAdded
-    //DuringPLAYINGStateBothPlayersHaveAtLeastOnePoint
+    @Property public void OneMovePlayedOnePieceAdded(@From(ReversiGenerator.class) Reversi r) {
+        char piece = (r.mIsBlackTurn) ? r.sBLACK_PIECE : r.sWHITE_PIECE;
+        int prePieceCount = getTotalPieceCount(r);
+        Agent.MoveCoord move = r.mAIAgent.findMove(r.getBoard(), piece);
+        if (move != null) {
+            Reversi.effectMove(r.mBoard, piece, move.getRow(), move.getCol());
+            int newPieceCount = getTotalPieceCount(r);
+            assertEquals(prePieceCount + 1, newPieceCount);
+        }
+    }
+
+    //BothPlayersHaveAtLeastOnePointInPlayingState
+    @Property public void BothPlayersHaveAtLeastOnePointInPlayingState(@From(ReversiGenerator.class) Reversi r) {
+        if (r.mState == r.PLAYING) {
+            assertTrue(r.getBlackScore() >= 1);
+            assertTrue(r.getWhiteScore() >= 1);
+        }
+    }
+
 //Magnus
     //APieceIntersectingAnotherSameColoredPieceInStraightLineConvertsAllPiecesInBetween (wbw => www)
     //InvalidMoveShouldNotAffectBoard (reversi.isValidMove can be made use of here)
